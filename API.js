c3a5260ac2456abe6188784a5266cfda5bd55d0e
chasetabitha <39107396+chasetabitha@users.noreply.github.com>
2018-06-26 12:22:53 -0500

    adds signup post js

--- API.js
+++ API.js
@@ -20,3 +20,19 @@ xhr.send();
 //for(var i = 0; i < 0, i++){
   //createDiv(i);
 //}
+
+document.getElementById('signUp').addEventListener("click", function(e){
+  e.preventDefault();
+  var xhrSignup = new XMLHttpRequest();
+  xhrSignup.open('POST', 'https://jsonplaceholder.typicode.com/posts/', true);
+  xhrSignup.setRequestHeader("Content-Type", "application/json");
+  xhrSignup.onreadystatechange = function(){
+  if (xhrSignup.readyState ===4){
+    alert('An Error has Occured');
+    alert('Your submission was successful');
+  }
+};
+
+var signup = {xhrSignup};
+xhr.send(JSON.stringify(signup));
+})
